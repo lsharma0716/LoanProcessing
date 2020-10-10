f9ea4d2c4430961223a6dc98139163aaa662b884
lsharma0716 <65988248+lsharma0716@users.noreply.github.com>
2020-10-10 13:52:43 -0500

    Create caller.py

---
+++ caller.py
@@ -0,0 +1,7 @@
+import func1 as getloan
+
+CustName = input('Enter Your name: ')
+Score = int(input('Enter Your Score: '))
+ReqAmt = int(input('Enter requested Loan Amount: '))
+
+getloan.LoanProcess(CustName,Score,ReqAmt)
