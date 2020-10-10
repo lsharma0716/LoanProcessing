e13630106cd7708bfc01bd0029a36d70ce5dbc7d
lsharma0716 <65988248+lsharma0716@users.noreply.github.com>
2020-10-10 15:06:10 -0500

    Update func1.py

--- func1.py
+++ func1.py
@@ -2,10 +2,14 @@ from loanmetadata import data
 
 
 def LoanProcess(CustName,Score,ReqAmt):
-    if Score in range(50,400) and ReqAmt >=10000:
+    if Score in range(50,400) and ReqAmt >= 10000 and ReqAmt<=40000:
         print(f'Congratulations {CustName} you are approved for ${ReqAmt} You can also consider:')
         for criteria in data:
             if Score in range(criteria['CS_Start']+1,criteria['CS_End']+1) and ReqAmt in range(criteria['Loan']+1):
                 print(f''' ${criteria['Loan']} at {criteria['Interest']}% interest for {criteria['Duration']} Months''')
+    elif ReqAmt < 10000 or ReqAmt > 40000:
+        print('Please enter amount in between $10000 to $40000')
+    elif Score < 50:
+        print('Request Denied low score')
     else:
-        print('Your request is Denied')
+        print('Your request is Denied. Credit Score out of range')
