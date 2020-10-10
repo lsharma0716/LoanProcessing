9935504d19eb032208cde14acca18905918b5bf7
lsharma0716 <65988248+lsharma0716@users.noreply.github.com>
2020-10-10 13:51:04 -0500

    Create laonmetadata.py

---
+++ laonmetadata.py
@@ -0,0 +1,14 @@
+#Meta Data for loan processing system
+
+data = [{'CS_Start':0,  'CS_End': 199,'Loan': 10000,'Duration':36,'Interest': 5},
+        {'CS_Start':200,'CS_End': 299,'Loan': 10000,'Duration':36,'Interest': 4},
+        {'CS_Start':300,'CS_End': 399,'Loan': 10000,'Duration':36,'Interest': 3},
+        {'CS_Start':0,  'CS_End': 199,'Loan': 20000,'Duration':36,'Interest': 5},
+        {'CS_Start':200,'CS_End': 299,'Loan': 20000,'Duration':36,'Interest': 4},
+        {'CS_Start':300,'CS_End': 399,'Loan': 20000,'Duration':36,'Interest': 3},
+        {'CS_Start':0,  'CS_End': 199,'Loan': 30000,'Duration':36,'Interest': 5},
+        {'CS_Start':200,'CS_End': 299,'Loan': 30000,'Duration':36,'Interest': 4},
+        {'CS_Start':300,'CS_End': 399,'Loan': 30000,'Duration':36,'Interest': 3},
+        {'CS_Start':0,  'CS_End': 199,'Loan': 40000,'Duration':36,'Interest': 5},
+        {'CS_Start':200,'CS_End': 299,'Loan': 40000,'Duration':36,'Interest': 4},
+        {'CS_Start':300,'CS_End': 399,'Loan': 40000,'Duration':36,'Interest': 3}]
