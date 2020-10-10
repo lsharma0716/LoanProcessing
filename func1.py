b2e2eee8da89942996996e9ddfd9d554e0bc1675
lsharma0716 <65988248+lsharma0716@users.noreply.github.com>
2020-10-10 14:03:17 -0500

    Update func1.py

--- func1.py
+++ func1.py
@@ -3,9 +3,9 @@ from loanmetadata import data
 
 def LoanProcess(CustName,Score,ReqAmt):
     if Score in range(50,400) and ReqAmt >=10000:
-        print(f'Congratulations {CustName} you are approved for ${ReqAmt} You can also consider')
-        for d in data:
-            if Score in range(d['CS_Start']+1,d['CS_End']+1) and ReqAmt in range(d['Loan']+1):
-                print(f''' ${d['Loan']} at {d['Interest']}% interest''')
+        print(f'Congratulations {CustName} you are approved for ${ReqAmt} You can also consider:')
+        for criteria in data:
+            if Score in range(criteria['CS_Start']+1,criteria['CS_End']+1) and ReqAmt in range(criteria['Loan']+1):
+                print(f''' ${criteria['Loan']} at {criteria['Interest']}% interest for {criteria['Duration']} Months''')
     else:
         print('Your request is Denied')
