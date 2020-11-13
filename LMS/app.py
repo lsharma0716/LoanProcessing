491090e48a7f5e8df5d753b55d8200b2650a994b
lsharma0716 <65988248+lsharma0716@users.noreply.github.com>
2020-11-13 10:16:39 -0600

    Create app.py

---
+++ LMS/app.py
@@ -0,0 +1,33 @@
+import dbconn
+import loadempmdm as emdm
+#import loadloanmdm lmdm
+#import loaneng le
+import sys
+
+args_list = sys.argv
+print(args_list)
+conn = None
+
+conn = dbconn.getConn(args_list[2])
+
+print(dbconn.g_conn_status)
+
+#
+if dbconn.g_conn_status == "active":
+    if args_list[1].upper() == 'EMPDATA':
+        LoadEmpObj = emdm.loadempmdm(conn,args_list[3])
+        pass
+#    elif args_list[1].upper() == 'LOANDATA':
+#        LoadEmpObj = lmdm.loadempmdm(conn,args_list[3])
+#        pass
+#    elif args_list[1].upper() == 'PROCESSLOAN':
+#        LoadEmpObj = le.loaneng(conn,args_list[3])
+#        pass
+else:
+    print("*** Fatal Error getting DB Conn! ***")
+
+# Run Modes
+# ########################################################
+# python app.py EMPDATA dbconfig.json appconfig.json
+# python app.py LOANDATA dbconfig.json appconfig.json
+# python app.py PROCESSLOAN dbconfig.json appconfig.json
